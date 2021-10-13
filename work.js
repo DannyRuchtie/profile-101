0fc889285bb204726a6e62b2c89289a4ab99bc1b
Danny Ruchtie <druchtie@gmail.com>
2021-10-13 11:50:56 +0000

    backup

---
+++ work.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+function Work(props) {
+  return (
+
+    <div className="workpage">
+
+<h1>Notable work and contributions</h1>
+
+<div className="cardHolder">
+    <div className="card">
+      <h2>CodeSandbox</h2>
+      <p>Leading a talented team of designers that shapes the next generation of web development tools.</p>
+      <a href="http://codesandbox.io">Visit CodeSandbox</a>
+    </div>
+
+    <div className="card">
+      <h2>IBM iX</h2>
+      <p>As Studio lead,  I  the ambitions of IBM and iX design Studios Benelux. I was overseeing a team—Building best-in-class digital experience for some of the world's biggest brands. </p>
+      <a target="_blank" href="https://www.ibm.com/services/ibmix">Visit IBM</a>
+    </div>
+
+    <div className="card">
+      <h2>Smart Ready to</h2>
+      <p>I worked with IBM, Daimler, and Computer Rock on the smart ready to platform and apps. Ready to plays a crucial part role in Smart's future vision. </p>
+      <a  target="_blank" href="https://future.smart.com/en/concept1.html?campid=intcmp_smartCom_IAA_headerNavigation_20210829">Download App</a>
+    </div>
+
+    <div className="card">
+      <h2>Catawiki</h2>
+      <p>Led the design of Catawiki's mobile platform. And worked on the Redesign of the main application catawiki.com</p>
+      <a target="_blank" href="https://www.catawiki.com/en/stories/5029-discover-treasures-with-the-catawiki-app">Download the apps</a>
+    </div>
+
+    <div className="card">
+      <h2>Albelli</h2>
+      <p>Led the design of  Albelli's is a mobile app.  With the Albelli photo book app for your smartphone, you can quickly and intuitively create your photo book. </p>
+      <a target="_blank" href="https://www.bonusprint.co.uk/apps">Download the apps</a>
+    </div>
+
+    <div className="card">
+      <h2>Lifelines</h2>
+        <p>Designed and Developed the web application LifeLines, a data visualization for the LifeLines study. A study into heritability and the environment affect the health of a human being.  username: Lifelines psw: Maaktonderzoekmogelijk.</p>
+      <a target="_blank" href="https://inzicht.lifelines.nl/index.php">Explore the app</a>
+    </div>
+
+    <div className="card">
+      <h2>Tango Demasque</h2>
+      <p>Edited and co-Produced the award-winning Tango Démasqué commercial commissioned by the Philips business-unit up-market flat TV a commercial to demonstrate the range of possibilities of the Philips Aurea Ambilight series.</p>
+      <a target="_blank"href="https://vimeo.com/75477866">Watch the video</a>
+    </div>
+
+    <div className="card">
+  
+      <h2>Perfect Pixel HD Engine  </h2>
+      <p>Edited and post-production for the Perfect Pixel HD Engine. Announcement video commissioned by the Philips business unit.</p>
+      <a target="_blank" href="https://vimeo.com/76327254">Watch the video</a>
+    </div>
+
+
+
+
+</div>
+
+    </div>  
+
+    );
+}
+
+export default Work;
